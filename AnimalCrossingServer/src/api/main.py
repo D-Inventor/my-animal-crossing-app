c51bc72a59d43b1ebc394fe13608ceda36f5b078
Dennis Heutinck <Dennis.Heutinck@outlook.com>
2026-02-15 12:59:07 +0200

    Remove all server code for a clean start

--- AnimalCrossingServer/src/api/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from .features.amiibo import router as amiibo
-from .features.characters import router as characters
-
-app = FastAPI()
-
-app.include_router(amiibo.router)
-app.include_router(characters.router)
